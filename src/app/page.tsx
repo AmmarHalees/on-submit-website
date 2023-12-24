97e183035956677baf86ec200483762209fa1351
Ammar Halees <haleesammar@gmail.com>
2023-12-24 20:22:40 +0300

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import UncontrolledForm from "@/components/UncontrolledForm/UncontrolledForm";
 export default function Home() {
   return (
     <main>
+      <h1 className="mt-8 mb-4 text-2xl xl:text-4xl text-center w-fit mx-auto">
+        <a target="_blank" href="https://github.com/AmmarHalees/onsubmit">
+          onsubmit{" "}
+        </a>
+      </h1>
       <UncontrolledForm />
     </main>
   );
